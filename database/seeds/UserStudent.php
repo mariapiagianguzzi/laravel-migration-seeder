7ac73bf91a85e168de140e543fffb5892019eb09
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-02-22 15:14:47 +0100

    laravel-migration-seeder inizio

---
+++ database/seeds/UserStudent.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserStudent extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
